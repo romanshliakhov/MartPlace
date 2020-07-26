341971a71f596e65f563fa495ddec157dcfcb847
romanshliakhov <romanshliakhov@gmail.com>
2020-07-26 18:26:10 +0300

    working with products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
     
+
+    
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        fullStar: true,
+        starWidth: "15px",
+        spacing: "3px",
+        normalFill: "#A0A0A0",
+    });
+    
     
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,7 @@ gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss') 
            .pipe(sass({outputStyle:'compressed'}))
            .pipe(rename({suffix : '.min'}))
-           .pipe(autoprefixer({
-            overrideBrowserslist: ['last 8 versions']
-           }))
+           .pipe(autoprefixer({overrideBrowserslist: ['last 8 versions']}))
            .pipe(gulp.dest('app/css'))  
            .pipe(browserSync.reload({stream: true}))      
 });
@@ -22,22 +20,24 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css',
     ])
-        .pipe(concat('libs.min.css')) 
-        .pipe(cssmin())       
-        .pipe(gulp.dest('app/css'))
+            .pipe(concat('libs.min.css')) 
+            .pipe(cssmin())       
+            .pipe(gulp.dest('app/css'))
 }); 
 
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
     ])
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'))
+            .pipe(concat('libs.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('app/js'))
 }); 
 
 gulp.task('html', function(){
