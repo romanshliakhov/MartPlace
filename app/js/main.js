16903307a6f13b6d925e9ddcda438521399978d6
romanshliakhov <romanshliakhov@gmail.com>
2020-07-28 00:17:10 +0300

    fixed products

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,10 @@
 $(function(){
-    
-
-    
+        
     $(".rate-star").rateYo({
         rating: 5,        
         fullStar: true,
         starWidth: "15px",
         spacing: "3px",
         normalFill: "#A0A0A0",
-    });
-    
-    
-
-
-
+    });  
 });
