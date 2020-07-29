ba820a14a202b7a8ab68074bc4da1e465a329864
romanshliakhov <romanshliakhov@gmail.com>
2020-07-29 12:07:38 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,20 @@
-$(function(){
-    
-    
-
-
-
-    
+$(function(){       
     $(".rate-star").rateYo({
         rating: 5,        
         fullStar: true,
         starWidth: "15px",
         spacing: "3px",
         normalFill: "#A0A0A0",
-    }); 
+    });    
+
+    $('.weekly__slider').slick({        
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        appendArrows: $('.weekly__inner-arrows'),
+        nextArrow: '<button type="submit" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="submit" class="slick-btn slick-prev"></button>',             
+    });
     
     var mixer = mixitup('.products__catalogue');
 });
